62c66d19b69adb56741256b701ce6a36765924f4
Brett Holcomb <ready.set.get.brett@gmail.com>
2025-02-05 14:00:10 -0800

    inlinable cards to customize colors

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,16 +1,26 @@
 <script>
   export let card = "2J";
+  // dynamically import the correct card component
+  $: cardComponent = async () => {
+    const module = await import(`./cards/${card}.svelte`);
+    return module.default;
+  };
 </script>
 
-<img class="card" src="/cards/{card}.svg" alt={card} />
+<div class="card-wrapper">
+  {#await cardComponent() then Component}
+    <svelte:component this={Component} />
+  {/await}
+</div>
 
 <style>
-  img {
+  .card-wrapper {
     position: relative;
+    width: 100vw;
     max-width: 12vw;
     max-height: 21vh;
     aspect-ratio: 2.5/3.5;
-    /* filter: invert(0.85); */
     filter: drop-shadow(0 0 0.4rem #4c350660);
+    border-radius: 1em;
   }
 </style>
